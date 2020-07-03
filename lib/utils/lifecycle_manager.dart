db860ca41689ac56464030232b04e5c3173f861b
lordvins226 <kevineilboudo54@gmail.com>
2020-07-03 11:15:32 +0000

    :star: new: add utils

---
+++ lib/utils/lifecycle_manager.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+
+class LifeCycleManager extends StatefulWidget {
+  final Widget child;
+  LifeCycleManager({Key key, this.child}) : super(key: key);
+
+  _LifeCycleManagerState createState() => _LifeCycleManagerState();
+}
+
+class _LifeCycleManagerState extends State<LifeCycleManager>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print('Etat = $state');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: widget.child,
+    );
+  }
+}
---
+++ lib/utils/responsive_builder.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_app/utils/sizing_information.dart';
+import 'package:tic_tac_app/utils/ui_utils.dart';
+
+class ResponsiveBuilder extends StatelessWidget {
+  final Widget Function(
+    BuildContext context,
+    SizingInformation sizingInformation,
+  ) builder;
+
+  const ResponsiveBuilder({Key key, this.builder}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, boxConstraints) {
+      var mediaQuery = MediaQuery.of(context);
+      var sizingInformation = SizingInformation(
+        deviceType: getDeviceType(mediaQuery),
+        screenSize: mediaQuery.size,
+        localWidgetSize:
+            Size(boxConstraints.maxWidth, boxConstraints.maxHeight),
+      );
+      return builder(context, sizingInformation);
+    });
+  }
+}
